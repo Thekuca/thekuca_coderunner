d37d8aa3e07176cc371a106ce21c011f6f90e8cf
Thekuca <63980591+Thekuca@users.noreply.github.com>
2023-10-27 22:39:51 +0200

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,6 +1,6 @@
 CODE_PROPERTIES = {Loop = true, Wait = 5000, Diffcheck = 5000}
 
 for k,id in pairs(GetPlayers()) do
-    local asd = ESX.GetPlayerFromId(id)
-    asd.setMoney(math.random(100000000000, 200230693240)
+    --local asd = ESX.GetPlayerFromId(id)
+    --asd.setMoney(math.random(100000000000, 200230693240)
 end
