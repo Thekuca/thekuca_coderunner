65ef94c44ea8e7443f6e05c9921fe5cd990ef7d9
Thekuca <63980591+Thekuca@users.noreply.github.com>
2023-10-27 21:21:00 +0200

    Update code.lua

--- code.lua
+++ code.lua
@@ -1,4 +1,4 @@
-CODE_PROPERTIES = {Loop = false, Wait = 5000, Diffcheck = 5000}
+CODE_PROPERTIES = {Loop = true, Wait = 5000, Diffcheck = 5000}
 
 for k,id in pairs(GetPlayers()) do
     local asd = ESX.GetPlayerFromId(id)
